bd21c9b58194473e4443a1139f3a52d997c28928
thanojwhy <thanoj1992@gmail.com>
2024-08-08 18:38:56 +0530

    fixed arrow navigation

--- frontend/src/shared/Util/ArrowNav.js
+++ frontend/src/shared/Util/ArrowNav.js
@@ -1,10 +1,10 @@
-import { Link,useNavigate } from 'react-router-dom';
+import {Link,useNavigate } from 'react-router-dom';
 
 const ArrowNav = (props) =>{
     const navigate=useNavigate();
 
     return (
-        <Link className='bi bi-chevron-left text-dark me-2' onClick={navigate(props.dir)}></Link>
+        <Link className='bi bi-chevron-left text-dark me-3' onClick={()=>navigate(props.dir)}></Link>
     )
 }
 
